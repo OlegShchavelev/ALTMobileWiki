1664c45b47cb8afc60cb1dbeb0b3f5aef6b7789b
Олег Щавелев <o.shchavelev@yandex.ru>
2024-09-18 15:39:19 +0300

    refactor: улучшена шаблонизация компонента «Cпонсоры» на главной странице проекта

--- .vitepress/theme/components/AMWHomeSponsors.vue
+++ .vitepress/theme/components/AMWHomeSponsors.vue
@@ -1,18 +1,21 @@
 <script setup lang="ts">
-import { computed } from 'vue'
-import { useData } from 'vitepress'
 import { VPHomeSponsors } from 'vitepress/theme'
+import { useData } from '../composables/data'
 
-const { frontmatter } = useData()
-const props = computed(() => frontmatter.value.sponsors ?? {})
+const { frontmatter: fm } = useData()
 </script>
 
 <template>
   <VPHomeSponsors
-    v-if="props.collections"
-    :actionText="props.donationtext"
-    :actionLink="props.donationlink"
-    :message="props.introtext"
-    :data="props.collections"
+    v-if="fm.sponsors.data"
+    :message="fm.sponsors.message"
+    :data="fm.sponsors.data"
   />
 </template>
+
+<style scoped>
+.VPHomeSponsors:deep(.VPSponsorsGrid.big .vp-sponsor-grid-image) {
+  max-width: 170px;
+  max-height: 120px;
+}
+</style>
